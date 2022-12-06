c39a7ad49d27c308f525a12d0f89932761baa387
Ephemeral <90786729+Nadim147c@users.noreply.github.com>
2022-12-06 23:33:46 +0600

    Fix: Update `commands` module.
    Use continue instead of using if-else statement to avoid possible nesting.

--- src/modules/commands.ts
+++ src/modules/commands.ts
@@ -23,50 +23,7 @@ export default async (client: ExtendedClient) => {
 
             const { Subcommand, SubcommandGroup } = ApplicationCommandOptionType
 
-            if (!filter(basePath) && (await client.isDir(basePath))) {
-                const groupFiles = readdirSync(basePath)
-
-                for (const group of groupFiles) {
-                    const groupPath = `${basePath}/${group}`
-
-                    let groupData: ApplicationCommandOption = {
-                        type: SubcommandGroup,
-                        name: group,
-                        description: `${group} related commands.`,
-                        options: [],
-                    }
-
-                    if (!filter(groupPath) && (await client.isDir(basePath))) {
-                        const subCommandFiles = readdirSync(groupPath)
-
-                        for (const sub of subCommandFiles) {
-                            const subCommandPath = `${groupPath}/${sub}`
-                            const command: CommandType = await client.importFile(subCommandPath)
-
-                            const name = `${file}-${group}-${command.data.name}`
-
-                            command.category = dir
-
-                            client.commands.set(name, command)
-
-                            groupData.options.push(Object.assign(command.data, { type: Subcommand }))
-                        }
-                    } else {
-                        const command: CommandType = await client.importFile(groupPath)
-
-                        command.category = dir
-
-                        const name = `${file}-${command.data.name}`
-
-                        client.commands.set(name, command)
-
-                        groupData = Object.assign(command.data, { type: Subcommand })
-                    }
-
-                    data.options?.push(groupData)
-                }
-            } else {
-                // eslint-disable-next-line no-await-in-loop
+            if (filter(basePath) || !(await client.isDir(basePath))) {
                 const command: CommandType = await client.importFile(basePath)
 
                 command.category = dir
@@ -74,6 +31,56 @@ export default async (client: ExtendedClient) => {
                 client.commands.set(command.data.name, command)
 
                 data = command.data
+
+                client.commandData.add(data)
+                // eslint-disable-next-line no-continue
+                continue
+            }
+
+            const groupFiles = readdirSync(basePath)
+
+            for (const group of groupFiles) {
+                const groupPath = `${basePath}/${group}`
+
+                let groupData: ApplicationCommandOption = {
+                    type: SubcommandGroup,
+                    name: group,
+                    description: `${group} related commands.`,
+                    options: [],
+                }
+
+                if (filter(groupPath) || !(await client.isDir(basePath))) {
+                    const command: CommandType = await client.importFile(groupPath)
+
+                    command.category = dir
+
+                    const name = `${file}-${command.data.name}`
+
+                    client.commands.set(name, command)
+
+                    groupData = Object.assign(command.data, { type: Subcommand })
+
+                    data.options?.push(groupData)
+                    // eslint-disable-next-line no-continue
+                    continue
+                }
+
+                const subCommandFiles = readdirSync(groupPath)
+
+                for (const sub of subCommandFiles) {
+                    const subCommandPath = `${groupPath}/${sub}`
+                    const command: CommandType = await client.importFile(subCommandPath)
+
+                    const name = `${file}-${group}-${command.data.name}`
+
+                    command.category = dir
+
+                    client.commands.set(name, command)
+
+                    groupData.options.push(Object.assign(command.data, { type: Subcommand }))
+                }
+
+                data.options?.push(groupData)
             }
 
             client.commandData.add(data)
