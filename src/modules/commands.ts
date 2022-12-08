f0dbee2b29167a58571ff6bf38205f333ed9a47a
Ephemeral <90786729+Nadim147c@users.noreply.github.com>
2022-12-08 10:53:20 +0600

    Fix: Replace `fs` to `glob` in command handler
    Change `fs` to `glob` to avoid nesting and increase speed. It also avoid unnecessary directory checking.

--- src/modules/commands.ts
+++ src/modules/commands.ts
@@ -1,89 +1,116 @@
-/* eslint-disable no-await-in-loop */
-/* eslint-disable no-restricted-syntax */
-import { ApplicationCommandOption, ApplicationCommandOptionType, ChatInputApplicationCommandData } from "discord.js"
-import { readdirSync } from "fs"
-import ExtendedClient from "../structures/Client"
-import { CommandType } from "../typings/Commands"
+import {
+    ApplicationCommandOptionType as OptionType,
+    ApplicationCommandSubGroupData,
+    ApplicationCommandType,
+    ChatInputApplicationCommandData,
+    Collection,
+} from "discord.js"
+import glob from "glob"
+import type ExtendedClient from "../structures/Client"
+import type { CommandType } from "../typings/Commands"
+
+type CommandPathInfo = {
+    path: string
+    file: string
+    category: string
+    subName: string
+    rootName: string
+}
+type CommandData = Collection<string, ChatInputApplicationCommandData>
+
+const path = `${__dirname}/../commands/`
+
+function extractInfoFromPath(PATH: string): CommandPathInfo {
+    const pieces = PATH.slice(path.length).split("/")
+
+    if (pieces.length < 2)
+        // eslint-disable-next-line max-len, @typescript-eslint/no-throw-literal
+        throw "Invalid files structure. See the docs: 'https://github.com/Nadim147c/Discord-handler#subcommands-and-files-structure'"
+
+    const info: CommandPathInfo = {
+        path: PATH,
+        category: pieces.shift(),
+        file: pieces.pop(),
+        subName: pieces.pop(),
+        rootName: pieces.pop(),
+    }
+
+    return info
+}
+
+const defaultBaseCommandData = (name: string): ChatInputApplicationCommandData => ({
+    name,
+    type: ApplicationCommandType.ChatInput,
+    description: `Commands related to ${name}.`,
+    options: [],
+})
+
+const defaultSubCommandData = (name: string): ApplicationCommandSubGroupData => ({
+    name,
+    type: OptionType.SubcommandGroup,
+    description: `Commands related to ${name}.`,
+    options: [],
+})
+
+function setCommandData(info: CommandPathInfo, module: CommandType, collection: CommandData) {
+    let commandData = collection.get(info.rootName)
+
+    if (info.rootName) {
+        if (!commandData) commandData = defaultBaseCommandData(info.rootName)
+
+        let groupData = commandData.options.find((CMD) => CMD.name === info.subName) as ApplicationCommandSubGroupData
+
+        if (!groupData) {
+            groupData = defaultSubCommandData(info.subName)
+            commandData.options.push(groupData)
+        }
+
+        const subcommand = groupData.options.find((x) => x.name === module.data.name)
+
+        if (!subcommand) groupData.options.push(Object.assign(module.data, { type: OptionType.Subcommand }))
+
+        collection.set(info.rootName, commandData)
+
+        return
+    }
+
+    if (info.subName) {
+        if (!commandData) commandData = defaultBaseCommandData(info.subName)
+
+        const subcommand = commandData.options.find((x) => x.name === module.data.name)
+
+        if (!subcommand) commandData.options.push(Object.assign(module.data, { type: OptionType.Subcommand }))
+
+        collection.set(info.subName, commandData)
+        return
+    }
+
+    if (commandData) return
+
+    collection.set(module.data.name, module.data)
+}
 
 export default async (client: ExtendedClient) => {
-    const path = `${__dirname}/../commands`
-    readdirSync(path).forEach(async (dir: string) => {
-        const filter = (file: string) => file.endsWith(".ts") || file.endsWith(".js")
-        const files = readdirSync(`${path}/${dir}`)
-        const commandFiles = files
+    async function registerCommands(_, files: string[]) {
+        const commandDataCollection: CommandData = new Collection()
 
-        for (const file of commandFiles) {
-            const basePath = `${path}/${dir}/${file}`
+        // eslint-disable-next-line no-restricted-syntax
+        for await (const file of files) {
+            const info = extractInfoFromPath(file)
 
-            let data: ChatInputApplicationCommandData = {
-                name: file,
-                description: `Commands related to ${file}`,
-                options: [],
-            }
+            const module: CommandType = await client.importFile(info.path)
 
-            const { Subcommand, SubcommandGroup } = ApplicationCommandOptionType
+            module.category = info.category
 
-            if (filter(basePath) || !(await client.isDir(basePath))) {
-                const command: CommandType = await client.importFile(basePath)
+            const identifier = [info.rootName, info.subName, module.data.name].filter((x) => x).join("-")
 
-                command.category = dir
+            client.commands.set(identifier, module)
 
-                client.commands.set(command.data.name, command)
-
-                data = command.data
-
-                client.commandData.add(data)
-                // eslint-disable-next-line no-continue
-                continue
-            }
-
-            const groupFiles = readdirSync(basePath)
-
-            for (const group of groupFiles) {
-                const groupPath = `${basePath}/${group}`
-
-                let groupData: ApplicationCommandOption = {
-                    type: SubcommandGroup,
-                    name: group,
-                    description: `${group} related commands.`,
-                    options: [],
-                }
-
-                if (filter(groupPath) || !(await client.isDir(basePath))) {
-                    const command: CommandType = await client.importFile(groupPath)
-
-                    command.category = dir
-
-                    const name = `${file}-${command.data.name}`
-
-                    client.commands.set(name, command)
-
-                    groupData = Object.assign(command.data, { type: Subcommand })
-
-                    data.options?.push(groupData)
-                    // eslint-disable-next-line no-continue
-                    continue
-                }
-
-                const subCommandFiles = readdirSync(groupPath)
-
-                for (const sub of subCommandFiles) {
-                    const subCommandPath = `${groupPath}/${sub}`
-                    const command: CommandType = await client.importFile(subCommandPath)
-
-                    const name = `${file}-${group}-${command.data.name}`
-
-                    command.category = dir
-
-                    client.commands.set(name, command)
-
-                    groupData.options.push(Object.assign(command.data, { type: Subcommand }))
-                }
-
-                data.options?.push(groupData)
-            }
-
-            client.commandData.add(data)
+            setCommandData(info, module, commandDataCollection)
         }
-    })
+
+        commandDataCollection.forEach((x) => client.commandData.add(x))
+    }
+
+    glob(`${path}**/**/**/*{.js,.ts}`, { windowsPathsNoEscape: true }, registerCommands)
 }
